41f98d327544375d71b26b1ff74fdaf761e9f401
SJ3rbi <99045574+SJ3rbi@users.noreply.github.com>
2022-02-17 20:19:51 +0000

    Add files via upload

---
+++ MyConnexion.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.esprit.utils;
+import java.sql.*;
+/**
+ *
+ * @author sk4nd
+ */
+public class MyConnexion {
+    
+    public String url="jdbc:mysql://localhost:3306/db_sponsors";
+    public String login="root";
+    public String pwd="";
+    Connection cnx;
+    public static MyConnexion instance;
+
+    private MyConnexion() {
+        try {
+            cnx = DriverManager.getConnection(url, login, pwd);
+            System.out.println("Connexion etablie!");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+    
+        public static MyConnexion getInstance() {
+        if(instance == null){
+            instance = new MyConnexion();
+        }
+        return instance;
+    }
+    public Connection getCnx(){
+        return cnx;
+    }
+    
+    
+}
